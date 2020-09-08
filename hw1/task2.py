0062562c5d48c001dca145e3b41e0dc0a91ddf30
bantsov-dmitriy <bantsov@gmail.com>
2020-09-09 00:15:32 +0300

    оптимизация формата вывода результата

--- hw1/task2.py
+++ hw1/task2.py
@@ -1,13 +1,10 @@
 # задача №2
 time_sec = int(input("Введите время в секундах: "))
-hours = str(time_sec // 3600)
+hours = time_sec // 3600
 delta_hours = time_sec % 3600
-minutes = str(delta_hours // 60)
-seconds = str(delta_hours % 60)
-if len(hours) < 2:
-    hours = '0' + hours
-if len(minutes) < 2:
-    minutes = '0' + minutes
-if len(seconds) < 2:
-    seconds = '0' + seconds
-print(f"Время в формате (чч:мм:сс): {hours}:{minutes}:{seconds}")
+minutes = delta_hours // 60
+seconds = delta_hours % 60
+print("Время в формате (чч:мм:сс): "
+      + '{:02}'.format(hours) + ':'
+      + '{:02}'.format(minutes) + ':'
+      + '{:02}'.format(seconds))
