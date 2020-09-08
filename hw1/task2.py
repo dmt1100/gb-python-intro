f0d738ba00249e6524e897727bc2bf5d75365287
bantsov-dmitriy <bantsov@gmail.com>
2020-09-08 22:48:10 +0300

    доработка файла

---
+++ hw1/task2.py
@@ -0,0 +1,7 @@
+# задача №2
+time_sec = int(input("Введите время в секундах: "))
+hours = time_sec // 3600
+delta_hours = time_sec % 3600
+minutes = delta_hours // 60
+seconds = delta_hours % 60
+print(f"Время в формате (чч:мм:сс): {hours}:{minutes}:{seconds}")
