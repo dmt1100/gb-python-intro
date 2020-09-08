604807bf22d79cc6d3918e542410cc8073fea7f4
bantsov-dmitriy <bantsov@gmail.com>
2020-09-08 23:43:26 +0300

    доработка task2.py - при выводе добавляем 0, если результат 1 знак

--- hw1/task2.py
+++ hw1/task2.py
@@ -1,7 +1,13 @@
 # задача №2
 time_sec = int(input("Введите время в секундах: "))
-hours = time_sec // 3600
+hours = str(time_sec // 3600)
 delta_hours = time_sec % 3600
-minutes = delta_hours // 60
-seconds = delta_hours % 60
+minutes = str(delta_hours // 60)
+seconds = str(delta_hours % 60)
+if len(hours) < 2:
+    hours = '0' + hours
+if len(minutes) < 2:
+    minutes = '0' + minutes
+if len(seconds) < 2:
+    seconds = '0' + seconds
 print(f"Время в формате (чч:мм:сс): {hours}:{minutes}:{seconds}")
