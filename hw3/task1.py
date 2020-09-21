78ac9aa94f7eebdbf5d3a18c451eae0963932c1d
bantsov-dmitriy <bantsov@gmail.com>
2020-09-21 07:54:05 +0300

    доработка задачи №1

--- hw3/task1.py
+++ hw3/task1.py
@@ -2,10 +2,10 @@
 def div(s1, s2):
     try:
         s1, s2 = int(s1), int(s2)
-        div_num = s1 / s2
+        result = s1 / s2
     except ZeroDivisionError:
         return "Делить на ноль нельзя!"
-    return round(div_num, 2)
+    return round(result, 2)
 
 n1 = input("Введите первое число: ")
 n2 = input("Введите второе число: ")
