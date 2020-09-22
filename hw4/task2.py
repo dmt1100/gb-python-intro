a9432221438a3d7a1e861c71e6b231edd5a59f5e
bantsov-dmitriy <bantsov@gmail.com>
2020-09-22 23:41:19 +0300

    решение задачи №2

---
+++ hw4/task2.py
@@ -0,0 +1,4 @@
+# задача №2
+my_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+print(f"Исходный список: {my_list}")
+print(f"Результат: {[my_list[num] for num in range(1, len(my_list)) if my_list[num] > my_list[num - 1]]}")
