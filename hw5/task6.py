4ee4bef0fd530f86043b2930f4e425705d8bce56
bantsov-dmitriy <bantsov@gmail.com>
2020-10-05 23:36:30 +0300

    решение задачи 6

---
+++ hw5/task6.py
@@ -0,0 +1,7 @@
+# задача №6
+with open('file6.txt', 'r', encoding='utf-8') as f:
+    a = f.readlines()
+    for s in a:
+        new_str = ''.join((i if i in '1234567890' else ' ') for i in s)
+        new_2 = [int(i) for i in new_str.split()]
+        print(f'{s.split()[0]} {sum(new_2)}')
