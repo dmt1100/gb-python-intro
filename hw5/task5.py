774cf52c4cd7e3b9263d86d7b6717a630212a46b
bantsov-dmitriy <bantsov@gmail.com>
2020-10-05 23:28:11 +0300

    решение задачи 5

---
+++ hw5/task5.py
@@ -0,0 +1,11 @@
+# задача №5
+from random import randint
+
+sum_el = 0
+with open("file5.txt", "w", encoding="utf-8") as my_file:
+    for i in range(100):
+        el = randint(1, 100)
+        sum_el += el
+        my_file.write(str(el) + " ")
+
+print(f"Сумма элементов = {sum_el}")
